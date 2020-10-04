f645f09ac3069c196ae3b29125a5fabba1d03fe5
Ariel Pillemer <63328798+pillemer@users.noreply.github.com>
2020-10-04 18:19:00 +1000

    Add files via upload
    added nose colour, backtrack covering and solution path tracing atfer completion

--- maze_maker.py
+++ maze_maker.py
@@ -15,8 +15,8 @@ clock = pygame.time.Clock()
 
 # colours
 BLACK = (0,   0,   0  )
-WHITE = (255, 255, 255)
-BLUE =  (0,   0,   255)
+WHITE = (192, 192, 192)
+BLUE =  (0,   0,   128)
 RED =   (255, 0,   0  )
 GREEN = (0,   255, 0  )
 
@@ -30,7 +30,7 @@ visited = []
 path = {}
 
 
-
+# ------ grid builder
 def build_grid(cell_size, grid_size):
     for i in range(grid_size):
         for j in range(grid_size):
@@ -39,7 +39,9 @@ def build_grid(cell_size, grid_size):
                                             cell_size, cell_size), 1)
             grid.append((i * (cell_size), j * (cell_size)))
 
+# ------ maze builder
 def create_maze(x, y):
+    draw_nose(x, y)
     visited.append((x,y))
     stack.append((x,y))
 
@@ -58,16 +60,20 @@ def create_maze(x, y):
                 grow_right(x,y)
             else:
                 grow_left(x,y) 
+
+            path[(cell_x, cell_y)] = (x, y)
             time.sleep(0.05)   
 
             create_maze(cell_x, cell_y)
 
         else:
+            # traceback
             x, y = stack.pop()
+            draw_backtrack(x, y)
+            cover_your_tracks(x, y)
 
 
-# ------   set up functions ------- #
-
+# ------ set up functions
 def check_neighbourhood(x,y):
     neighbours = []
     if (x + cell_size, y) in grid and (x + cell_size, y) not in visited:
@@ -81,17 +87,22 @@ def check_neighbourhood(x,y):
 
     return neighbours
 
+# ------ draw direction indicators:
 def draw_nose(x, y):
     pygame.draw.rect(SCREEN, RED, (x + 1, y + 1, cell_size - 2, cell_size - 2))
     pygame.display.update()
-    time.sleep(0.1) 
+    time.sleep(0.05) 
 
 def draw_backtrack(x, y):
     pygame.draw.rect(SCREEN, GREEN, (x + 1, y + 1, cell_size - 2, cell_size - 2))
     pygame.display.update()
-    time.sleep(0.1)
+    time.sleep(0.05)
 
-# draw maze movemenet:
+def cover_your_tracks(x, y):
+    pygame.draw.rect(SCREEN, BLUE, (x + 1, y + 1, cell_size - 2, cell_size - 2))
+    pygame.display.update()
+
+# ------ draw maze movemenet:
 def grow_right(x, y):
     pygame.draw.rect(SCREEN, BLUE, (x + 1, y + 1,  2 * cell_size - 2, cell_size - 2))
     pygame.display.update()
@@ -108,16 +119,30 @@ def grow_up(x, y):
     pygame.draw.rect(SCREEN, BLUE, (x + 1, y - cell_size + 1, cell_size - 2, 2 * cell_size - 2))
     pygame.display.update()
 
+def draw_solution(x, y):
+    while (x,y) != grid[0]:
+        pygame.draw.circle(SCREEN, RED, (x + cell_size // 2, y + cell_size // 2), 2 , 0)
+        pygame.display.update()
+        time.sleep(0.05)
+        x, y = path[x, y]
 
+
+# ------ make it go
 build_grid(cell_size, grid_size)
-create_maze(0,0)
+create_maze(0,0)  # start the maze in the top left corner
+
+# draw the maze solution starting from the bottom right corner
+
+x,y = grid[len(grid)-1]
+draw_solution(x,y)
 
 
-# pygame loop
+# ------ pygame loop
 running = True
 while running:
     clock.tick(FPS)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
+            pygame.quit()
             running = False
     pygame.display.update()
